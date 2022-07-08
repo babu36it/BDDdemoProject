24631c40f9f1dbde18113c2ce25b6194e664ced2
babu36it <108249158+babu36it@users.noreply.github.com>
2022-07-08 08:47:38 +0530

    Update WebReusability.java

--- src/test/java/Utilities/WebReusability.java
+++ src/test/java/Utilities/WebReusability.java
@@ -1,10 +1,16 @@
 package Utilities;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class WebReusability extends BaseClass{
@@ -13,6 +19,8 @@ public class WebReusability extends BaseClass{
 
 //    BaseClass baseClass= new BaseClass();
 
+    private String windowHandle;
+
     public void waitUntilElementVisible(WebElement element){
 
         WebDriverWait wait = new WebDriverWait(driver, 100);
@@ -20,5 +28,44 @@ public class WebReusability extends BaseClass{
         wait.until(ExpectedConditions.visibilityOf(element));
 
     }
+
+    public void switchToNewWindow(){
+
+        windowHandle= driver.getWindowHandle();
+
+        Set<String> windowHandles=driver.getWindowHandles();
+
+        for(String handle:windowHandles){
+
+            if(!handle.equals(windowHandle)){
+
+                System.out.println("The window reference is: "+handle);
+
+                driver.switchTo().window(handle);
+
+            }
+
+        }
+
+    }
+
+    public void switchBackParentWindow(){
+
+        driver.switchTo().window(windowHandle);
+
+    }
+
+    public void takeScreenshot(String expectedImageName) throws IOException {
+
+        TakesScreenshot screenshot = (TakesScreenshot)driver;
+
+        File source=screenshot.getScreenshotAs(OutputType.FILE);
+
+        File fis= new File("src/test/java/Utilities/Screenshots/"+expectedImageName+".png");
+
+        FileUtils.copyFile(source,fis);
+
+    }
+
 
 }
